7a9a98f4f390e5954d25efc4f62946eca33012ad
Eric <ericjohnlavoie@gmail.com>
2020-03-14 23:18:23 -0400

    std::i64::MAX and bool

--- src/types.rs
+++ src/types.rs
@@ -8,7 +8,27 @@ Characters (char)
 Tuples
 Arrays
 */
-
+/* Rust is a satically typed language, which means that it must know the  types
+of all variables at compile time, however, the compiler  can usually infer what 
+type we want to use based on the value and how we use it. 
+*/
 pub fn run() {
+    println!("Hello from types::run()");
+
+    // Default is "i32"
+    let x = 1;
+
+    // Float Default is f64 
+    let y = 2.5;
+
+    // Add explicit type
+    let z :i64 = 4545454545454;
     
+    println!("Max is i32 {}", std::i32::MAX);
+    println!("Max is i64 {}", std::i64::MAX);
+    println!("{} default i32, {} default f64, explicit :i64 {}", x, y, z);
+
+    // Boolean
+    let is_active: bool = true;
+    println!("{:?}", (x, y, z, is_active));
 }
